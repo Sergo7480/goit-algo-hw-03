f997e661ee189ae953c710f3c7d880bac4a8d41c
Sergo7480 <157243352+Sergo7480@users.noreply.github.com>
2024-03-12 05:10:47 +0200

    Update import datetime.py

--- import datetime.py
+++ import datetime.py
@@ -1,9 +1,12 @@
 from datetime import datetime
-def get_days_from_today(date: int) -> int:
-    input_date = datetime.strptime(date, '%Y-%m-%d')
-    current_date = datetime.today()
-    days_delta = (current_date - input_date).days
-    return days_delta
+def get_days_from_today(date):
+    try:
+        input_date = datetime.strptime(date, '%Y-%m-%d')
+        current_date = datetime.today()
+        days_delta = (current_date - input_date).days
+        return days_delta
+    except ValueError:
+        return "Неправильный формат даты. Введите дату в формате 'ГГГГ-ММ-ДД'."
 input_date = "1980-04-07"
 result = get_days_from_today(input_date)
 print(f"Количество дней от {input_date} до текущей даты: {result}")
