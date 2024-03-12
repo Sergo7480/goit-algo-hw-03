b042734783aad3e941c419d6bb515d7c7a8071fc
Sergo7480 <157243352+Sergo7480@users.noreply.github.com>
2024-03-12 21:07:42 +0200

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -15,36 +15,27 @@ def find_next_weekday(d, weekday: int):
         days_ahead += 7  
     return d + timedelta(days=days_ahead)  
 
+def get_upcoming_birthdays(users):
+    today = datetime.today().date()
+    upcoming_birthdays = []
 
+    for user in users:
+        try:
+            birthday = datetime.strptime(user['birthday'], '%Y.%m.%d').date()
+            birthday_this_year = birthday.replace(year=today.year)
 
-prepared_users = []  
-for user in users: 
-    try:
-        birthday = datetime.strptime(user['birthday'], '%Y.%m.%d').date()  
-        prepared_users.append({"name": user['name'], 'birthday': birthday})  
-    except ValueError:
-        print(f'Некоректна дата народження для користувача {user["name"]}')  
+            if birthday_this_year < today:
+                birthday_this_year = birthday_this_year.replace(year=today.year + 1)
 
+            if 0 <= (birthday_this_year - today).days <= 7:
+                if birthday_this_year.weekday() >= 5:
+                    birthday_this_year = find_next_weekday(birthday_this_year, 0)
 
-
-days = 7  
-today = datetime.today().date()  
-
-upcoming_birthdays = []  
-for user in prepared_users:  
-    birthday_this_year = user["birthday"].replace(year=today.year)  
-
-    if birthday_this_year < today:  
-        birthday_this_year = birthday_this_year.replace(year=today.year + 1)  
-
-    if 0 <= (birthday_this_year - today).days <= days:  
-        if birthday_this_year.weekday() >= 5:  
-            birthday_this_year = find_next_weekday(birthday_this_year, 0)  
-
-        congratulation_date_str = birthday_this_year.strftime('%Y.%m.%d')  
-        upcoming_birthdays.append({  
-            "name": user["name"],
-            "congratulation_date": congratulation_date_str
-        })
-
-print(upcoming_birthdays)  
+                congratulation_date_str = birthday_this_year.strftime('%Y.%m.%d')
+                upcoming_birthdays.append({
+                    "name": user["name"],
+                    "congratulation_date": congratulation_date_str
+                })
+        except ValueError:
+            print(f'Некоректна дата народження для користувача {user["name"]}')
+    return upcoming_birthdays
